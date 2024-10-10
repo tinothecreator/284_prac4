2d015135dfe40a420168ad88f067a0c9b318c36d
DevanDewet <161315233+DevanDewet@users.noreply.github.com>
2024-10-10 18:12:01 +0200

    75 submission

--- task1_student_struct.asm
+++ task1_student_struct.asm
@@ -1,8 +1,9 @@
 ; ==========================
-; Group member 01: Name_Surname_student-nr
-; Group member 02: Name_Surname_student-nr
-; Group member 03: Name_Surname_student-nr
+; Group member 01: Tinotenda_Chirozvi_22547747
+; Group member 02: Tafara_Hwata_22565991
+; Group member 03: Devan_Dewet_05169098
 ; ==========================
+
 
 extern malloc
 section .text
--- task3_student_linkedlist.asm
+++ task3_student_linkedlist.asm
@@ -1,105 +1,86 @@
 ; ==========================
-; Group member 01: Name_Surname_student-nr
-; Group member 02: Name_Surname_student-nr
-; Group member 03: Name_Surname_student-nr
+; Group member 01: Tinotenda_Chirozvi_22547747
+; Group member 02: Tafara_Hwata_22565991
+; Group member 03: Devan_Dewet_05169098
 ; ==========================
 
 section .data
-    debug_msg db "Debug: Added student to list - ID: %d, Name: %s, GPA: %f", 10, 0
 
 section .text
 global addStudentToList
 extern malloc
-extern printf
 
 addStudentToList:
     push rbp
     mov rbp, rsp
-    sub rsp, 32  ; Allocate stack space for local variables
+    sub rsp, 32
 
-    ; Save parameters
-    mov [rsp], rdi      ; head pointer
-    mov [rsp + 8], rsi  ; name pointer
-    movss [rsp + 16], xmm0 ; GPA
+    mov [rsp], rdi
+    mov [rsp + 8], rsi
+    movss [rsp + 16], xmm0
 
-    ; Allocate memory for new StudentNode (16 bytes)
     mov rdi, 16
     call malloc
     test rax, rax
     jz .allocation_failed
 
-    mov [rsp + 24], rax ; Save new StudentNode pointer
+    mov [rsp + 24], rax
 
-    ; Allocate memory for new Student (72 bytes)
     mov rdi, 72
     call malloc
     test rax, rax
     jz .allocation_failed
 
-    ; rax now points to new Student struct
-    mov rdx, [rsp + 24] ; Get StudentNode pointer
-    mov [rdx], rax      ; Set StudentNode->student
+    mov rdx, [rsp + 24]
+    mov [rdx], rax
 
-    ; Find highest ID and last node
-    mov r12, [rsp]      ; r12 = head pointer
-    mov r12, [r12]      ; r12 = *head
-    xor r13d, r13d      ; r13d = highest ID
-    mov r14, r12        ; r14 = last node
+    mov r12, [rsp]
+    mov r12, [r12]
+    xor r13d, r13d
+    mov r14, r12
 
 .traverse_list:
     test r12, r12
     jz .set_new_student
 
-    mov r15d, [r12]     ; Load current Student's ID
+    mov r15d, [r12]
     cmp r15d, r13d
-    cmovg r13d, r15d    ; Update highest ID if current is greater
+    cmovg r13d, r15d
 
-    mov r14, r12        ; Update last node
-    mov r12, [r12 + 8]  ; Move to next node
+    mov r14, r12
+    mov r12, [r12 + 8]
     jmp .traverse_list
 
 .set_new_student:
-    ; Set new ID (highest + 1)
     inc r13d
     mov [rax], r13d
 
-    ; Set name (next 64 bytes)
-    mov rsi, [rsp + 8]  ; Source: name pointer
-    lea rdi, [rax + 4]  ; Destination: name field in struct
-    mov rcx, 63         ; Copy up to 63 characters
+    mov rsi, [rsp + 8]
+    lea rdi, [rax + 4]
+    mov rcx, 63
     cld
     rep movsb
-    mov byte [rdi], 0   ; Null-terminate the string
+    mov byte [rdi], 0
 
-    ; Set GPA (last 4 bytes)
     movss xmm0, [rsp + 16]
     movss [rax + 68], xmm0
 
-    ; Set next pointer to NULL
-    mov rdx, [rsp + 24] ; Get StudentNode pointer
+    mov rdx, [rsp + 24]
     mov qword [rdx + 8], 0
 
-    ; Update list
-    mov rcx, [rsp]      ; Get head pointer
+    mov rcx, [rsp]
     cmp qword [rcx], 0
     jne .update_last_node
 
-    ; If list was empty, set head
     mov [rcx], rdx
-    jmp .print_debug
+    jmp .end_function
 
 .update_last_node:
     mov [r14 + 8], rdx
 
-.print_debug:
-    ; Debug print
-    mov rdi, debug_msg
-    mov esi, [rax]      ; ID
-    lea rdx, [rax + 4]  ; Name
-    movss xmm0, [rax + 68] ; GPA
-    cvtss2sd xmm0, xmm0   ; Convert float to double for printf
-    mov al, 1           ; One floating point argument
-    call printf
+.end_function:
+    leave
+    ret
 
 .allocation_failed:
     leave
